34a48dfbaa38d29084c450549d6a0a88358a7c4a
Isaac Velando <isaac.velando@ibm.com>
2021-02-04 20:18:23 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # telegraf-input-zpool-status
-A simple tool to extract zpool status and output Influx line protocol
+
+A simple tool to extract zpool status and output Influx line protocol; it is
+designed to be used with a [telegraf exec plugin](https://github.com/influxdata/telegraf/tree/master/plugins/inputs/exec). This
+parses the output of `zpool -H -p` and has been developed against Ubuntu 20.04
+with ZFS 0.8.3 and InfluxDB 1.x for generating compatible line protocol.
+
+As an example:
+
+```
+zpool,alternative_root=-,name=testpool allocated=127488i,capacity=0i,checkpoint=0i,dedup=1,expand=0i,fragmentation=0i,free=204009671
+68i,health=3i,size=20401094656i 1612490775507703812
+zpool,alternative_root=-,name=testpool111 allocated=122880i,capacity=0i,checkpoint=0i,dedup=1,expand=0i,fragmentation=0i,free=204009
+71776i,health=3i,size=20401094656i 1612490775507703812
+```
+
+Once https://github.com/influxdata/telegraf/pull/6724 is merged the
+`zpool -H -p` functionality would be redundant and a native telegraf plugin
+could be used.
+
+# Future Work
+
+For zpool health monitoring I also want to monitor the read, write, and checksum
+errors reported by `zpool status`; that functionality is not currently planned
+in the aforementioned pull request, so that would remain useful.
---
+++ main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/influxdata/influxdb1-client"
+	influxdb "github.com/influxdata/influxdb1-client"
+	"go.uber.org/zap"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var statusInt = map[string]int{
+	"DEGRADED": 0,
+	"FAULTED":  1,
+	"OFFLINE":  2,
+	"ONLINE":   3,
+	"REMOVED":  4,
+	"UNAVAIL":  5,
+}
+
+func main() {
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		fmt.Println("{\"op\": \"main\", \"level\": \"fatal\", \"msg\": \"failed to initiate logger\"}")
+		os.Exit(1)
+	}
+	defer logger.Sync()
+
+	cmd := exec.Command("zpool", "list", "-H", "-p")
+	ts := time.Now()
+	stdout, err := cmd.Output()
+	if err != nil {
+		logger.Error("failed to execute zpool list -H -p",
+			zap.String("op", "main"),
+			zap.Error(err),
+		)
+		return
+	}
+
+	columns := strings.Fields(string(stdout))
+	if len(columns)%11 != 0 {
+		logger.Error("unexpected number of columns in output",
+			zap.String("op", "main"),
+			zap.Error(err),
+		)
+		return
+	}
+
+	for i := 0; i < len(columns)/11; i++ {
+		poolName := columns[11*i]
+		poolSize, err := strconv.Atoi(columns[11*i+1])
+		if err != nil {
+			logger.Error("failed to convert pool size to int",
+				zap.String("op", "main"),
+				zap.Error(err),
+			)
+			return
+		}
+		poolAlloc, err := strconv.Atoi(columns[11*i+2])
+		if err != nil {
+			logger.Error("failed to convert pool allocated size to int",
+				zap.String("op", "main"),
+				zap.Error(err),
+			)
+			return
+		}
+		poolFree, err := strconv.Atoi(columns[11*i+3])
+		if err != nil {
+			logger.Error("failed to convert pool free size to int",
+				zap.String("op", "main"),
+				zap.Error(err),
+			)
+			return
+		}
+		var poolCheckpoint int
+		poolCheckpointRaw := columns[11*i+4]
+		if poolCheckpointRaw == "-" {
+			poolCheckpoint = 0
+		} else {
+			poolCheckpoint, err = strconv.Atoi(poolCheckpointRaw)
+			if err != nil {
+				logger.Error("failed to convert pool checkpoint size to int",
+					zap.String("op", "main"),
+					zap.Error(err),
+				)
+				return
+			}
+		}
+		var poolExpandSize int
+		poolExpandSizeRaw := columns[11*i+5]
+		if poolExpandSizeRaw == "-" {
+			poolExpandSize = 0
+		} else {
+			poolExpandSize, err = strconv.Atoi(poolExpandSizeRaw)
+			if err != nil {
+				logger.Error("failed to convert pool expand size to int",
+					zap.String("op", "main"),
+					zap.Error(err),
+				)
+				return
+			}
+		}
+		poolFragmentation, err := strconv.Atoi(columns[11*i+6])
+		if err != nil {
+			logger.Error("failed to convert pool fragmentation to int",
+				zap.String("op", "main"),
+				zap.Error(err),
+			)
+			return
+		}
+		poolCapacity, err := strconv.Atoi(columns[11*i+7])
+		if err != nil {
+			logger.Error("failed to convert pool capacity to int",
+				zap.String("op", "main"),
+				zap.Error(err),
+			)
+			return
+		}
+		poolDedup, err := strconv.ParseFloat(columns[11*i+8], 32)
+		if err != nil {
+			logger.Error("failed to convert pool dedup ratio to float",
+				zap.String("op", "main"),
+				zap.Error(err),
+			)
+			return
+		}
+		var poolHealth int
+		var ok bool
+		poolHealthRaw := columns[11*i+9]
+		if poolHealth, ok = statusInt[poolHealthRaw]; !ok {
+			poolHealth = 0
+		}
+		poolAltRoot := columns[11*i+10]
+
+		data := influxdb.Point{
+			Measurement: "zpool",
+			Tags: map[string]string{
+				"name":             poolName,
+				"alternative_root": poolAltRoot,
+			},
+			Fields: map[string]interface{}{
+				"size":          poolSize,
+				"allocated":     poolAlloc,
+				"free":          poolFree,
+				"checkpoint":    poolCheckpoint,
+				"expand":        poolExpandSize,
+				"fragmentation": poolFragmentation,
+				"capacity":      poolCapacity,
+				"dedup":         poolDedup,
+				"health":        poolHealth,
+			},
+			Time:      ts,
+			Precision: "ns",
+		}
+		fmt.Println(data.MarshalString())
+	}
+
+}
